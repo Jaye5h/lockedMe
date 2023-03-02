4c10c122d05b1d7339e62b9c9d9d6062c2de8dab
Jayesh Waghmare <jayeshwaghmare476@gmail.com>
2023-03-02 21:36:25 +0530

    Initial commit

---
+++ src/com/project/lockedme/Main.java
@@ -0,0 +1,15 @@
+package com.project.lockedme;
+
+
+public class Main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\ACER ASPIRE 7\\Desktop\\JayeshWaghmare_Phase1\\src\\com\\project\\lockedme";
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
